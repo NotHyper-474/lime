31eb311a151d649f05d8f6fefd8935ba873e66cf
Joshua Granick <jgranick@users.noreply.github.com>
2024-06-10 22:07:05 -0700

    Initial support for SDL_sound

--- project/src/ExternalInterface.cpp
+++ project/src/ExternalInterface.cpp
@@ -48,6 +48,10 @@
 #include <utils/compress/Zlib.h>
 #include <vm/NekoVM.h>
 
+#ifdef LIME_SDL_SOUND
+#include <media/decoders/SDL_sound.h>
+#endif
+
 #ifdef HX_WINDOWS
 #include <locale>
 #include <codecvt>
@@ -386,6 +390,14 @@ namespace lime {
 		bytes.Set (data);
 		resource = Resource (&bytes);
 
+		#ifdef LIME_SDL_SOUND
+		if (SDL_sound::Decode (&resource, &audioBuffer)) {
+
+			return audioBuffer.Value (buffer);
+
+		}
+		#endif
+
 		if (WAV::Decode (&resource, &audioBuffer)) {
 
 			return audioBuffer.Value (buffer);
@@ -408,6 +420,14 @@ namespace lime {
 	HL_PRIM AudioBuffer* HL_NAME(hl_audio_load_bytes) (Bytes* data, AudioBuffer* buffer) {
 
 		Resource resource = Resource (data);
+
+		#ifdef LIME_SDL_SOUND
+		if (SDL_sound::Decode (&resource, buffer)) {
+
+			return buffer;
+
+		}
+		#endif
 
 		if (WAV::Decode (&resource, buffer)) {
 
@@ -436,6 +456,14 @@ namespace lime {
 
 		resource = Resource (val_string (data));
 
+		#ifdef LIME_SDL_SOUND
+		if (SDL_sound::Decode (&resource, &audioBuffer)) {
+
+			return audioBuffer.Value (buffer);
+
+		}
+		#endif
+
 		if (WAV::Decode (&resource, &audioBuffer)) {
 
 			return audioBuffer.Value (buffer);
@@ -458,6 +486,14 @@ namespace lime {
 	HL_PRIM AudioBuffer* HL_NAME(hl_audio_load_file) (hl_vstring* data, AudioBuffer* buffer) {
 
 		Resource resource = Resource (data ? hl_to_utf8 ((const uchar*)data->bytes) : NULL);
+
+		#ifdef LIME_SDL_SOUND
+		if (SDL_sound::Decode (&resource, buffer)) {
+
+			return buffer;
+
+		}
+		#endif
 
 		if (WAV::Decode (&resource, buffer)) {
 
