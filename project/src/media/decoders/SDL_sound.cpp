d967b48cb17c2c4094958f28fd43213d2643a16b
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-04-06 00:40:25 -0300

    Fail if sound has Int32 data format

--- project/src/media/decoders/SDL_sound.cpp
+++ project/src/media/decoders/SDL_sound.cpp
@@ -9,20 +9,15 @@ namespace lime {
 	bool SDL_sound::Decode (Resource *resource, AudioBuffer *audioBuffer) {
 		Sound_Sample* sample = NULL;
 		// The OGG decoder also returns 16-bit signed samples so why not?
-		Sound_AudioInfo desired = {
-			0,//AUDIO_S16LSB,
-			0, // channels, blank for SDL_sound to determine
-			0, // sample ratio
-		};
 
 		if (resource->path) {
 
-			sample = Sound_NewSampleFromFile(resource->path, &desired, 65536);
+			sample = Sound_NewSampleFromFile(resource->path, NULL, 65536);
 
 		} else {
 
 			// FIXME?: WAV files require ext to work due to a bug in SDL_sound
-			sample = Sound_NewSampleFromMem(resource->data->b, resource->data->length, "wav", &desired, 65536);
+			sample = Sound_NewSampleFromMem(resource->data->b, resource->data->length, "wav", NULL, 65536);
 
 		}
 
@@ -51,9 +46,11 @@ namespace lime {
 				break;
 			case AUDIO_S32LSB:
 			case AUDIO_S32MSB:
-				audioBuffer->bitsPerSample = 32;
-				audioBuffer->dataFormat = 1;
-				break;
+				// No support for signed 32bit audio formats
+				audioBuffer->channels = 0;
+				audioBuffer->sampleRate = 0;
+				Sound_FreeSample(sample);
+				return false;
 
 			case AUDIO_U16LSB:
 			case AUDIO_S16LSB:
@@ -75,8 +72,6 @@ namespace lime {
 			Sound_FreeSample(sample);
 			return false;
 		}
-		// TODO: This seems a few bits short?
-		//Uint32 dataLength = (duration * audioBuffer->sampleRate * audioBuffer->channels * (audioBuffer->bitsPerSample / 8)) / 1000;
 
 		Uint8* bytes = NULL;
 		Uint32 bytesWritten = 0;
